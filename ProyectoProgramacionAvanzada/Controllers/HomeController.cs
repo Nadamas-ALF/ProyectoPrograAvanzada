5512a9a35ca23a66f8cef0044f240f9d18e13038
Esolano1129 <edgardoasolano@gmail.com>
2026-07-12 11:09:38 -0600

    Login Features
    Inicio de sesion, validaciones, hash de contraseñas.

--- ProyectoProgramacionAvanzada/Controllers/HomeController.cs
+++ ProyectoProgramacionAvanzada/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProyectoProgramacionAvanzada.EF;
+using ProyectoProgramacionAvanzada.Models;
+using ProyectoProgramacionAvanzada.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +15,55 @@ namespace ProyectoProgramacionAvanzada.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (Session["IdUsuario"] != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(new InicioSesionModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(InicioSesionModel Modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(Modelo);
+            }
+
+            using (var Servicio = new UsuarioService())
+            {
+                SP_ConsultarUsuarioInicioSesion_Result Usuario =
+                    Servicio.ValidarCredenciales(Modelo);
+
+                if (Usuario == null)
+                {
+                    ModelState.AddModelError(
+                        string.Empty,
+                        "El correo o la contraseña son incorrectos, o el usuario no esta activo."
+                    );
+
+                    return View(Modelo);
+                }
+
+                Session["IdUsuario"] = Usuario.IdUsuario;
+                Session["NombreUsuario"] =
+                    Usuario.Nombre + " " + Usuario.Apellido;
+                Session["IdRol"] = Usuario.IdRol;
+                Session["NombreRol"] = Usuario.NombreRol;
+                Session["IdEstado"] = Usuario.IdEstado;
+                Session["NombreEstado"] = Usuario.NombreEstado;
+
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
 
         public ActionResult About()
         {
---
+++ ProyectoProgramacionAvanzada/Models/UsuarioModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoProgramacionAvanzada.Models
+{
+    public class UsuarioModel
+    {
+
+        public int IdUsuario { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(100)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        [StringLength(100)]
+        [Display(Name = "Apellido")]
+        public string Apellido { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Cedula")]
+        public string Cedula { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Telefono")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El correo electronico es obligatorio.")]
+        [StringLength(150)]
+        [EmailAddress(ErrorMessage = "Ingrese un correo electronico valido.")]
+        [Display(Name = "Correo electronico")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contrasenna es obligatoria.")]
+        [StringLength(255)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contrasenna")]
+        public string Contrasenna { get; set; }
+
+        public int IdRol { get; set; }
+
+        public int IdEstado { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
